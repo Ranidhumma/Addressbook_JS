1ca689fb262f899066223dcc4db778ec5670ad44
Ranidhumma <ranidhumma8@gmail.com>
2022-04-09 00:39:14 +0530

    creted a addressbook contact

--- Addressbook.js
+++ Addressbook.js
@@ -1,1 +1,40 @@
-console.log ("Welcome to Addressbook program")
+console.log ("Welcome to Addressbook program");
+class Contact {
+firstName;
+lastName;
+address;
+city;
+state;
+zip;
+phoneNumber;
+email;
+
+constructor(firstName, lastName, address, city, state, zip, phoneNumber, email){
+this.firstName = firstName;
+this.lastName = lastName;
+this.address = address;
+this.city = city;
+this.state = state;
+this.zip = zip;
+this.phoneNumber = phoneNumber;
+this.email = email;
+}
+
+
+
+toString() {
+return "First name: " + this.firstName + "\nLast name: " + this.lastName +
+"\nAddress: " + this.address +  "\nCity: " + this.city +
+"\nState: " + this.state + "\nZip: " + this.zip +
+"\nPhone number: " + this.phoneNumber + "\nEmail: " + this.email;
+}}
+
+
+let contact = new Contact("Rani","Dhumma","Shelgi","Solapur","Maharashtra",12345,9999922222,"rd@gmail.com");
+
+console.log(contact.toString());
+
+
+
+
+
